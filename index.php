9533270df0d5ba067ecd2852ee76c0f57a299ebc
root <root@rosie-ThinkPad-X200s>
2021-09-20 12:40:21 +0100

    Remove jQuery link from the head in index file
    jQuery was used in experimenting with Ajax

--- index.php
+++ index.php
@@ -18,8 +18,6 @@ include 'php-functions/all-timestamps.php';
     <meta name="format-detection" content="telephone=no">
     <title>Searching system</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 
 <body class="body">
