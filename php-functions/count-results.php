4d796ccc8e4c0a10ec0f1ec9bf04ac7b2fc2ff5c
Andrzej Piontek <yendrrek@protonmail.com>
2021-10-02 20:55:26 +0100

    Show total number of events found by fields updated above the result box

--- php-functions/count-results.php
+++ php-functions/count-results.php
@@ -31,3 +31,44 @@ function showTotalNumberOfEventsByType()
 }
 
 showTotalNumberOfEventsByType();
+
+function showTotalNumberOfEventsByFieldsUpdated()
+{
+    $eventsByFieldsUpdated = [];
+    $qtyOfEventsByFieldsUpdatedFound = 0;
+    $infoAboutQtyOfEventsByFieldsUpdatedFound = $infoAboutQtyOfEventsFoundWithNoFieldsUpdated = '';
+
+    foreach(getEventFile() as $event) {
+
+        if (filter_var(!empty($_POST['fieldsUpdated']), FILTER_SANITIZE_STRING)) {
+
+            if (strpos($event, $_POST['fieldsUpdated']) !== false) {
+
+                array_push($eventsByFieldsUpdated, $event);
+            }
+        }
+    }
+
+    $qtyOfEventsByFieldsUpdatedFound = count($eventsByFieldsUpdated);
+
+    $infoAboutQtyOfEventsByFieldsUpdatedFound =
+      "{$qtyOfEventsByFieldsUpdatedFound} events found with updated field '{$_POST['fieldsUpdated']}'";
+
+    $infoAboutQtyOfEventsFoundWithNoFieldsUpdated =
+      "{$qtyOfEventsByFieldsUpdatedFound} events found with no fields updated";
+
+    if ($qtyOfEventsByFieldsUpdatedFound < 1) {
+
+        $infoAboutQtyOfEventsByFieldsUpdatedFound = null;
+
+    } elseif ($_POST['fieldsUpdated'] === 'null') {
+
+        echo $infoAboutQtyOfEventsFoundWithNoFieldsUpdated;
+
+    } else {
+
+        echo $infoAboutQtyOfEventsByFieldsUpdatedFound;
+    }
+}
+
+showTotalNumberOfEventsByFieldsUpdated();
