023b2a265f9afa069112448151fead0a3667dce0
Andrzej Piontek <yendrrek@protonmail.com>
2021-11-02 21:49:07 +0000

    Remove obsolete comments

--- php-functions/csrf-token.php
+++ php-functions/csrf-token.php
@@ -1,6 +1,4 @@
 <?php
-// Generate random token on each HTTP request for each
-// form to prevent Cross-Site Request Forgery.
 function createCsrfToken()
 {
     return $_SESSION['tokenCsrf'] = $_SESSION['tokenCsrf'] ?? bin2hex(random_bytes(64));
--- php-functions/form-validation.php
+++ php-functions/form-validation.php
@@ -1,6 +1,4 @@
 <?php
-// Token sent from a form is matched against the same token stored in $_SESSION.
-// If they do not match, form is not submitted.  
 function validateSearchForm()
 {
     if (!empty($_POST['tokenCsrf'])) {
